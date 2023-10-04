9416e1e36ae2423758b42d5313b4d6f8720788c6
abdullahHatem02 <90287364+abdullahHatem02@users.noreply.github.com>
2023-10-04 19:50:55 +0300

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const adminController = require('../controllers/adminController');
-const userController = require('../controllers/userController');
-const doctorController = require('../controllers/doctorController');
 const router = express.Router();
 
 router.route("/addAdmin").post(adminController.addAdmin);
