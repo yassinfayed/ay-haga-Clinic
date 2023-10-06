916546c2facbec7644b68f88e4c03ccc9cc78665
salsabilmonib <salsabillotfymonib@gmail.com>
2023-10-06 20:00:06 +0300

    initial commit

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -3,5 +3,7 @@ const doctorController = require('../controllers/doctorController');
 const router = express.Router();
 
 router.route("/getDoctor/:id").get(doctorController.getDoctor);
+router.route("/getallDoctors/:id").get(doctorController.getalldoctors);
+router.route("/updatedoctor/:id").post(doctorController.updateDoctor);
 
 module.exports = router;
