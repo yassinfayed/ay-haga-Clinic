20dc202900da0eb70071c58766bb66d333078be9
Saraahmedd <saraahmedsaber@gmail.com>
2023-11-07 01:47:36 +0200

    all filter sections in doctor done

--- frontend/src/app/doctor/patients/page.js
+++ frontend/src/app/doctor/patients/page.js
@@ -4,10 +4,8 @@ import { Table} from '../../../../components/Table';
 import { Button} from '../../../../components/Button'; 
 import './page.css' ;
 import { useDispatch, useSelector } from 'react-redux';
-import { filterPatientsBasedOnUpcomingAppointments, viewPatients } from '@/app/redux/actions/patientsActions';
-import { login } from '@/app/redux/actions/authActions';
-import { Navbar } from 'react-bootstrap';
-import NavbarDoc from '../../../../components/NavbarDoc';
+import { viewPatients } from '@/app/redux/actions/patientsActions';
+import Image from 'next/image';
 
 
 
@@ -41,33 +39,38 @@ function PatientsList() {
     dispatch(viewPatients({...name,...upcoming}))
   },[name,upcoming])
 
+  const handleClearFilters = () => {
+    setName(null);
+    setUpcoming(null);
+  }
+
  
   return (
     <div className='m-2'>
       <h3 className='my-1 mt-0 text-center text-title'>Patients</h3>
       <div className='underline-Bold mx-auto mb-5'></div>
-      <div className="div container-fluid d-flex ">
-      <div className="search-container">
-      <input
-        type="text"
-        placeholder="Search For Patient"
-        className="search-input"
-        onChange={(e) =>{ 
-          setName( {"name": e.target.value } ); 
-      }
-      } 
-      />
+      <div className='row flex align-items-center justify-content-start bg-light p-2 pe-0 m-3 rounded border'>
+        <div className="col-md-1">
+          <Image src='/filter.svg' height={30} width={30} className=""/>
+        </div>  
+        <div className="col-md-3">
+          <input
+            type="text"
+            placeholder="Patient Name"
+            className="form-control my-auto"
+            onChange={(e) =>{setName( {"name": e.target.value } );}} />
+        </div>
+        <div className="col-md-3 container-fluid" style={{ display: 'flex', alignItems: 'center' }}>
+          <label htmlFor="upcomingAppointments">Upcoming Appointments</label>
+          <input
+            onChange={(e) => {setUpcoming(e.target.checked ? {status:'Upcoming'}:{})}}
+            type="checkbox" id="upcomingAppointments" name="upAp" value="upAp" 
+            style={{ width: '20px', height: '20px', marginLeft: '10px' }} />
+        </div>
+        <div className="col-md-3 ms-auto">
+          <Button text="Clear Filters" className="w-60 ms-5" onClick={handleClearFilters} variant={'md'}></Button>       
+        </div>
       </div>
-      <div className="div container-fluid" style={{ display: 'flex', alignItems: 'center' }}>
-  <label htmlFor="upcomingAppointments">Upcoming Appointments</label>
-  <input
-   onChange={(e) => {
-    setUpcoming(e.target.checked ? {status:'Upcoming'}: {})  
-   }
-   }
-   type="checkbox" id="upcomingAppointments" name="upAp" value="upAp" style={{ width: '20px', height: '20px', marginLeft: '10px' }} />
-</div>
-</div>
     <div className=".patient-table-container">
     <Table headers={tableHeaders} data={tabledata}  itemsPerPageOptions={[5, 10, 15]} />
     </div>
