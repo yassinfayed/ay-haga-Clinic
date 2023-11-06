fda0a912ee830b58d2c5b14901008eecfa830cad
abdullahHatem02 <Abdullahhatem87@yahoo.com>
2023-11-07 00:43:46 +0200

    Auth requirements

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,6 +6,20 @@ const AppError = require("./../utils/appError");
 const enums = require("../constants/enums");
 const Patient = require("../models/patientModel");
 const Doctor = require("./../models/doctorModel");
+const Email = require('./../utils/email');
+const crypto = require('crypto');
+
+function generateOTP(length) {
+  const digits = '0123456789';
+  let otp = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = crypto.randomInt(0, digits.length);
+    otp += digits[randomIndex];
+  }
+
+  return otp;
+}
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -182,4 +196,105 @@ exports.login = catchAsync(async (req, res, next) => {
   
   createSendToken(user, 200, req, res);
 
+});
+
+
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+  res.status(200).json({ status: 'success' });
+};
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2) Check if POSTed current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
+
+  // 3) If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // User.findByIdAndUpdate will NOT work as intended!
+
+  // 4) Log user in, send JWT
+  createSendToken(user, 200, req, res);
+});
+
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+  let user;
+  let toBePassed;
+  if(req.body.role === 'patient') {
+    toBePassed = await Patient.findOne({ email:req.body.email });
+    user = await User.findOne({ _id: toBePassed.user });
+
+  }
+  else {
+    toBePassed = await Doctor.findOne({ email:req.body.email });
+    user = await User.findOne({ _id: toBePassed.user });
+
+  }
+  if (!user) {
+    return next(new AppError('There is no user with email address.', 404));
+  }
+
+  // 2) Generate the random reset token
+  const OTP = generateOTP(6);
+  user.OTP = OTP;
+  user.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  await user.save({ validateBeforeSave: false });
+
+  // 3) Send it to user's email
+
+  try {
+    await new Email(toBePassed, OTP).sendPasswordReset();
+
+    res.status(200).json({
+      status: 'success',
+      message: 'OTP sent to email!'
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    console.log(err)
+
+    return next(
+      new AppError('There was an error sending the email. Try again later!'),
+      500
+    );
+  }
+});
+
+
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+
+  const user2 = req.body.role === 'patient' ? await Patient.findOne({email: req.body.email}) : await Doctor.findOne({email: req.body.email}) 
+  const user = await User.findOne({
+    OTP: req.body.OTP,
+    _id: user2.user,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+
+  // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  // 3) Update changedPasswordAt property for the user
+  // 4) Log the user in, send JWT
+  createSendToken(user, 200, req, res);
 });
