cff4985ce6cf24eaa265872b716674fd96d0cb1e
Saraahmedd <saraahmedsaber@gmail.com>
2023-11-10 03:39:25 +0200

    package-subscription modal skeleton done

--- frontend/components/SubscribeModal.js
+++ frontend/components/SubscribeModal.js
@@ -1,8 +1,9 @@
-import { createHealthPackage, updateHealthPackage } from '@/app/redux/actions/healthPackagesActions';
+import './components.css';
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { useDispatch } from 'react-redux';
+import { Form, Col } from 'react-bootstrap';
+import Familymembers from '@/app/patient/Familymembers/page';
 
 
 function SubscribeModal(props) {
@@ -11,21 +12,36 @@ function SubscribeModal(props) {
     const dispatch=useDispatch();
     
     const [packageReciever, setPackageReciever] = useState('');
+    const [familyMember, setFamilyMember] = useState(null);
+
     const [paymentMethod, setPaymentMethod] = useState('');
+
+    const handleFamilyMemberChange = (e) => {
+        setFamilyMember(e.target.value);
+        console.log(e.target.value)
+    }
+
+    const handleRecieverChange = (e) => {
+        setPackageReciever(e.target.value);
+        console.log(e.target.value)
+    };
 
     const handleSubmit=(e)=>{
         e.preventDefault();
-
+        if(!packageReciever || (packageReciever=='family' && !familyMember)){
+            console.log('please enter all data')
+            return;
+        }
         //logic to be added
         // dispatch(func({
         // "data": data,
         // }))
+        setFamilyMember(null)
+        setPackageReciever(null)
+        console.log('submitted')
         props.onHide()
     }
 
-    const handlePackageRecieverChange = ()=>{
-        //logic to be added
-    }
 
     return (
         <Modal
@@ -33,18 +49,62 @@ function SubscribeModal(props) {
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
         centered>
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter text-primary text-bold text-center">
+            <Modal.Header closeButton className='bg-primary'>
+            </Modal.Header>
+            <Modal.Body className='bg-light'>
+                <Modal.Title id="contained-modal-title-vcenter" className='px-2 text-global text-bold text-center'>
                     {title}
                 </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
+                <div className='underline-Bold mx-auto mt-2 mb-5'></div>
                 <h4>{subheader}</h4>
                 <div>
-                <form onSubmit={(e)=>{handleSubmit(e)}}>
-                    {/* data fields to be added */}
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
+                <Form onSubmit={(e)=>{handleSubmit(e)}} className='text-semibold'>
+                    <Form.Group className='row m-2'>
+                        <Form.Label className='col-md-4'>Subscription reciever:</Form.Label>
+                        <div className='col-md-6'>
+                        <Form.Check
+                            inline
+                            type="radio"
+                            label="Me"
+                            name="radioOption"
+                            value="me"
+                            checked={packageReciever === 'me'}
+                            onChange={handleRecieverChange}
+                        />
+                        <Form.Check
+                            inline
+                            type="radio"
+                            label="Family Member"
+                            name="radioOption"
+                            value="family"
+                            checked={packageReciever === 'family'}
+                            onChange={handleRecieverChange}
+                        />
+                        </div>
+                    </Form.Group>
+                    
+                    <Form.Group className='row m-2'>
+                        <Form.Label className='col-md-4'>Select Family Member:</Form.Label>
+                        <div className='col-md-6'>
+                            <Form.Control
+                            as="select"
+                            disabled={packageReciever === 'me'} 
+                            className='col-md-4'
+                            onChange={handleFamilyMemberChange}
+                            required
+                            >
+                            <option value={null}>Choose...</option>
+                            <option value='Mom'>Mom</option>
+                            <option value='Dad'>Dad</option>
+                            <option value='Sis'>Sister</option>
+                            <option value='Bro'>Brother</option>
+                            </Form.Control>
+                        </div>
+                    </Form.Group>
+                    <div className="row justify-content-end align-items-center mt-5 mb-2">
+                        <button type="submit" className="btn btn-primary mx-auto col-md-2">Submit</button>
+                    </div>
+                </Form>
                 </div>
             </Modal.Body>
         </Modal>
