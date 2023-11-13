9d8d387c8067878423cce5fd307530c453e4f029
Hazem Abdelghany <hazemabdelghany43@gmail.com>
2023-11-13 15:12:58 +0200

    push

--- frontend/src/app/doctor/Appointments/page.js
+++ frontend/src/app/doctor/Appointments/page.js
@@ -1,25 +1,26 @@
-"use client"
-import React from 'react'
-import {Table} from '../../../../components/Table';
-import Navbar from '../../../../components/Navbar';
-import Footer from '../../../../components/Footer';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { login } from '@/app/redux/actions/authActions';
-import { useEffect } from 'react';
-import { useMemo } from 'react';
-import { Button } from '../../../../components/Button';
-import { doctorFollowUpAction, getDoctorAppointments } from '@/app/redux/actions/doctorActions';
+"use client";
+import React from "react";
+import { Table } from "../../../../components/Table";
+import Navbar from "../../../../components/Navbar";
+import Footer from "../../../../components/Footer";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "@/app/redux/actions/authActions";
+import { useEffect } from "react";
+import { useMemo } from "react";
+import { Button } from "../../../../components/Button";
+import {
+  doctorFollowUpAction,
+  getDoctorAppointments,
+} from "@/app/redux/actions/doctorActions";
 import NavbarDoc from "../../../../components/NavbarDoc";
 import FooterDoc from "../../../../components/FooterDoc";
-import Image from 'next/image';
-import { Modal, ModalBody, ModalHeader } from 'react-bootstrap';
+import Image from "next/image";
+import { Modal, ModalBody, ModalHeader } from "react-bootstrap";
 
 function docappointments() {
-
-
   const dispatch = useDispatch();
 
   const [selectedDate, setSelectedDate] = useState(null);
@@ -28,7 +29,6 @@ function docappointments() {
   const [modal, setModal] = useState(false);
   const [appt, setAppt] = useState(null);
 
-
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
@@ -36,37 +36,38 @@ function docappointments() {
     setSelectedStatus(event.target.value);
   };
 
-  const appointmentsData = useSelector((state) => state.viewDoctorsAppointmentsReducer.appointments);
-  const isLoading = useSelector((state) => state.viewDoctorsAppointmentsReducer.loading);
+  const appointmentsData = useSelector(
+    (state) => state.viewDoctorsAppointmentsReducer.appointments
+  );
+  const isLoading = useSelector(
+    (state) => state.viewDoctorsAppointmentsReducer.loading
+  );
 
   const formatDateToISOString = (date) => {
-    if (!date) return ''; // Return an empty string if date is falsy
-    const utcDate = new Date(Date.UTC(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate()
-    ));
-    const selected =  utcDate.toUTCString();
+    if (!date) return ""; // Return an empty string if date is falsy
+    const utcDate = new Date(
+      Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+    );
+    const selected = utcDate.toUTCString();
     return selected;
   };
 
   async function fetchData() {
-  
     const queryObj = {
       date: formatDateToISOString(selectedDate),
       status: selectedStatus,
     };
-  
+
     const filteredQueryObj = Object.keys(queryObj).reduce((acc, key) => {
-      if (queryObj[key] !== '') {
+      if (queryObj[key] !== "") {
         acc[key] = queryObj[key];
       }
       return acc;
     }, {});
-  
+
     dispatch(getDoctorAppointments(filteredQueryObj));
   }
-  
+
   useEffect(() => {
     fetchData();
   }, [dispatch, selectedDate, selectedStatus]);
@@ -78,50 +79,55 @@ function docappointments() {
     console.log(newDate);
   };
 
-  const handleFollowup = (id) =>{
+  const handleFollowup = (id) => {
     setModal(true);
     setAppt(id);
-  }
+  };
 
-  const handleFollowupSubmit = (id) =>{
-    dispatch(doctorFollowUpAction({appointmentId: id, date: newDate}))
-  }
-  
-  function FollowupModal( show, onHide ) {
+  const handleFollowupSubmit = (id) => {
+    dispatch(doctorFollowUpAction(id, newDate));
+  };
+
+  function FollowupModal(show, onHide) {
     return (
-      <Modal centered show={show} onHide={onHide} size="md">
-        <ModalHeader closeButton className='bg-primary'></ModalHeader>
-        <ModalBody className='bg-light'>
-        <Modal.Title id="contained-modal-title-vcenter" className='px-2 text-global text-bold text-center'>
-        Schedule A followup
-        </Modal.Title>
-        <hr />
-        <div className=" row col-md-12">
-          <div className="col-md-8">
-            <input
-              type="datetime-local"
-              id={`date`}
-              name="appointmentdate"
-              className="form-control"
-              onChange={(e) => {
-                handleFollowupDate(e);
+      <Modal centered show={show.show} onHide={show.onHide} size="md">
+        <ModalHeader closeButton className="bg-primary"></ModalHeader>
+        <ModalBody className="bg-light">
+          <Modal.Title
+            id="contained-modal-title-vcenter"
+            className="px-2 text-global text-bold text-center"
+          >
+            Schedule A followup
+          </Modal.Title>
+          <hr />
+          <div className=" row col-md-12">
+            <div className="col-md-8">
+              <input
+                type="datetime-local"
+                className="form-control py-2"
+                name="appointmentdate"
+                value={newDate}
+                onChange={(e) => {
+                  setNewDate(e.target.value);
+                }}
+              />
+            </div>
+            <Button
+              text="Schedule Followup"
+              id={`btn`}
+              variant="sm"
+              onClick={() => {
+                handleFollowupSubmit(appt);
+                console.log("hello");
               }}
-            />
+              className="col-md-4"
+              disabled={newDate === null ? true : false}
+            ></Button>
           </div>
-          <Button
-            text="Schedule Followup"
-            id={`btn`}
-            variant="sm"
-            onClick={()=>{handleFollowupSubmit(appt)}}
-            className='col-md-4'
-            disabled={newDate===null? true : false}
-          ></Button>
-        </div>
         </ModalBody>
       </Modal>
     );
   }
-
 
   const generateButton = (id) => {
     return (
@@ -129,8 +135,10 @@ function docappointments() {
         text="Schedule Followup"
         id={`btn`}
         variant="md"
-        onClick={()=>{handleFollowup(id)}}
-        className='col-md-6'
+        onClick={() => {
+          handleFollowup(id);
+        }}
+        className="col-md-6"
       ></Button>
     );
   };
@@ -138,65 +146,77 @@ function docappointments() {
   const handleClearFilters = () => {
     setSelectedDate(null);
     setSelectedStatus(null);
-  }
-  
-  const headers = ['Date', 'Patient Name', 'Status', 'Actions'];
+  };
+
+  const headers = ["Date", "Patient Name", "Status", "Actions"];
 
   const apps = useMemo(() => {
     if (appointmentsData && appointmentsData.data) {
       return appointmentsData.data.map((value) => ({
-        date: new Date(value.date).toLocaleDateString(), 
+        date: new Date(value.date).toLocaleDateString(),
         patientname: value.patientId?.name,
         status: value.status,
-        action: generateButton(value._id)
+        action: generateButton(value._id),
       }));
     }
     return [];
   }, [appointmentsData]);
 
-
-  return(
-    <div className='global-text'>
-      <NavbarDoc/>
-        <div className="container-fluid m-5">
+  return (
+    <div className="global-text">
+      <NavbarDoc />
+      <div className="container-fluid m-5">
         <div className="rows">
-            <h3 className='my-1 mt-0 text-center text-title'>My Appointments</h3>
-            <div className='underline-Bold mx-auto mb-5'></div>
-            <div className='row flex align-items-center justify-content-start bg-light p-2 pe-0 m-3 rounded border'>
-                  <div className="col-md-1">
-                  <Image src='/filter.svg' height={30} width={30} className=""/>
-                  </div>   
-                  <div className="status-filter col-md-3">
-                    <select onChange={handleStatusChange} className='w-100 form-control text-muted px-2'>
-                      <option value="">Filter by status</option>
-                      <option value="Completed">Completed</option>
-                      <option value="Upcoming">Upcoming</option>
-                      <option value="Missed">Missed</option>
-                      <option value="Cancelled">Cancelled</option>
-                      <option value="Rescheduled">Rescheduled</option>
-                    </select>
-                  </div>
-                  <div className="col-md-4 text-muted p-2">
-                    <DatePicker
-                      selected={selectedDate}
-                      onChange={handleDateChange}
-                      dateFormat="yyyy-MM-dd"
-                      placeholderText="Filter by date"
-                      className='form-control'
-                    />
-                  </div>
-                  <div className="col-md-3 ms-auto">
-                    <Button text="Clear Filters" className="w-60 ms-5" onClick={handleClearFilters} variant={'md'}></Button>       
-                  </div>
-              </div>
+          <h3 className="my-1 mt-0 text-center text-title">My Appointments</h3>
+          <div className="underline-Bold mx-auto mb-5"></div>
+          <div className="row flex align-items-center justify-content-start bg-light p-2 pe-0 m-3 rounded border">
+            <div className="col-md-1">
+              <Image src="/filter.svg" height={30} width={30} className="" />
+            </div>
+            <div className="status-filter col-md-3">
+              <select
+                onChange={handleStatusChange}
+                className="w-100 form-control text-muted px-2"
+              >
+                <option value="">Filter by status</option>
+                <option value="Completed">Completed</option>
+                <option value="Upcoming">Upcoming</option>
+                <option value="Missed">Missed</option>
+                <option value="Cancelled">Cancelled</option>
+                <option value="Rescheduled">Rescheduled</option>
+              </select>
+            </div>
+            <div className="col-md-4 text-muted p-2">
+              <DatePicker
+                selected={selectedDate}
+                onChange={handleDateChange}
+                dateFormat="yyyy-MM-dd"
+                placeholderText="Filter by date"
+                className="form-control"
+              />
+            </div>
+            <div className="col-md-3 ms-auto">
+              <Button
+                text="Clear Filters"
+                className="w-60 ms-5"
+                onClick={handleClearFilters}
+                variant={"md"}
+              ></Button>
+            </div>
+          </div>
         </div>
-        <FollowupModal show={modal} onHide={()=>{setModal(false); setAppt(null)}}/>
-        <Table headers={headers} data={apps ? apps : []}/>
+        <FollowupModal
+          show={modal}
+          onHide={() => {
+            setModal(false);
+            setAppt(null);
+          }}
+        />
+        <Table headers={headers} data={apps ? apps : []} />
       </div>
-      <FooterDoc/>
+      <FooterDoc />
     </div>
-      )
-      
+  );
 }
 
 export default docappointments;
