799b246dfd2562c44bd503933fbfce9422ff2d35
Saraahmedd <saraahmedsaber@gmail.com>
2023-11-27 02:06:16 +0200

    doctor profile fixes

--- frontend/src/app/doctor/[id]/page.js
+++ frontend/src/app/doctor/[id]/page.js
@@ -43,11 +43,9 @@ export default function DoctorProfile({ params }) {
 
   useEffect(() => {
     dispatch(viewDoctorDetails(params.id));
-    // dispatch(doctorViewContract(params.id));
   }, [dispatch, id, newEmail, newdoctor, isLoading]);
 
   
-//console.log(doctor?.employmentContract?.status)
   const docStatus = doctor?.employmentContract?.status;
 
   if (localStorage) {
@@ -121,9 +119,6 @@ export default function DoctorProfile({ params }) {
     setNewAffiliation("");
   };
 
-  // console.log(docStatus)
-  // console.log(doctor)
-
 
   function DateCardList() {
     return (
@@ -135,7 +130,8 @@ export default function DoctorProfile({ params }) {
               text={<div className="text-semibold text-center text-primary">{formatDateToDDMMYYYYHHMM(date)}</div>}
               onClick={() => alert("Card Clicked")}
               onClickButton={() => alert("Button Clicked")}
-              headerText={index + 1}
+              className='bg-white'
+              headerText='Available'
             />
           </div>
         ))}
@@ -162,12 +158,15 @@ export default function DoctorProfile({ params }) {
         <div className="m-5">
           <div className=" d-flex mx-auto rounded shadow col-md-9 my-5">
             <div className=" w-25 border-end ">
-              <div className="p-3 border-bottom mx-auto">
-                <div>
-                  <Image src="/profile.svg" height={200} width={200} />
-                </div>
+              <div className="image-container">
+                <Image 
+                  src="/profile.svg" 
+                  alt="Profile"
+                  layout="intrinsic"
+                  width={200}
+                  height={200}
+                />
               </div>
-              
               <div className="ps-4 py-2 d-flex  text-center border-bottom mx-auto">
                     <span className="fw-bold w-25 ">
                       <Image src="/dollar.svg" height={30} width={30} />
@@ -201,15 +200,16 @@ export default function DoctorProfile({ params }) {
               </div>
               <div className="p-2 border-bottom row">
                 <div className="p-3 col-md-6">
-                  <div className="text-body-secondary fw-bold small p-3 ">
+                  <div className="text-body-secondary fw-bold small">
                     Doctor Information
                   </div>
-                  <div className="py-3 d-flex">
+                  <hr className="w-50" />
+                  <div className="py-2 d-flex">
                     <span className="fw-bold w-25">
                       <Image src="/mail-dark.svg" height={25} width={25} />
                     </span>
-                    {edit || <span className="w-50">{doctor.email}</span>}
-                    <span className="w-50">
+                    {edit || <span className="w-70">{doctor.email}</span>}
+                    <span className="w-70">
                       {!edit || (
                         <input
                           type="email"
@@ -225,15 +225,14 @@ export default function DoctorProfile({ params }) {
                     <span className="fw-bold w-25 ">
                       <Image src="/birthday.svg" height={25} width={25} />
                     </span>
-                    <span className="w-50">{date}</span>
-                    <span className="w-50"></span>
+                    <span className="w-70">{date}</span>
                   </div>
                   <div className="py-2 d-flex ">
                     <span className="fw-bold w-25 ">
                       <Image src="/cart.svg" height={25} width={25} />
                     </span>
-                    {edit || <span className="w-50">{doctor.HourlyRate}</span>}
-                    <span className="w-50">
+                    {edit || <span className="w-70">{doctor.HourlyRate}</span>}
+                    <span className="w-70">
                       {!edit || (
                         <input
                           type="email"
@@ -249,8 +248,8 @@ export default function DoctorProfile({ params }) {
                     <span className="fw-bold w-25 ">
                       <Image src="/affiliation.svg" height={25} width={25} />
                     </span>
-                    {edit || <span className="w-50">{doctor.affiliation}</span>}
-                    <span className="w-50">
+                    {edit || <span className="w-70">{doctor.affiliation}</span>}
+                    <span className="w-70">
                       {!edit || (
                         <input
                           type="email"
@@ -266,8 +265,7 @@ export default function DoctorProfile({ params }) {
                     <span className="fw-bold w-25">
                       <Image src="/education.svg" height={25} width={25} />
                     </span>
-                    <span className="w-50">{doctor.educationalbackground}</span>
-                    <span className="w-50"></span>
+                    <span className="w-70">{doctor.educationalbackground}</span>
                   </div>
                 </div>
                 <hr />
@@ -293,12 +291,15 @@ export default function DoctorProfile({ params }) {
                   ></Button>
                 </div>
               )}
-                <div className="">
-                  <div className="text-body-secondary fw-bold small p-3 ">
+                <div className="mt-3">
+                  <div className="text-body-secondary fw-bold small">
                     Available Dates
                   </div>
-                  <div className=" row">
-                    <div className="col-md-8">
+                  <div className="w-50 mb-4">
+                    <hr className="w-50" />
+                  </div>
+                  <div className=" row mx-auto">
+                    <div className="col-md-6">
                     <input
                       type="datetime-local"
                       id="appointmentdate"
@@ -309,12 +310,12 @@ export default function DoctorProfile({ params }) {
                       }}
                     />
                     </div>
-                  <Button
-                    text="Add Available Date"
-                    variant="xs"
-                    onClick={() => handleAddDate()}
-                    className='col-md-4'
-                  ></Button>
+                    <Button
+                      text="Add Available Date"
+                      variant="xs"
+                      onClick={() => handleAddDate()}
+                      className='col-md-4'
+                    ></Button>
                   </div>
                   <br></br>
                   <DateCardList />
