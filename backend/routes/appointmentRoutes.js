cf5c1d72d096080ce5432236f0cc383c60bcd860
Amir-Ashraf1 <amir.ashraf16502@gmail.com>
2023-11-27 22:54:27 +0200

    BE only

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -14,6 +14,8 @@ router.get('/:id', appointmentController.getAppointment);
 //This will be deleted in sprints 2 & 3 as it will involve much more complexity, payment refund etc.../ Will be used for now to seed dummy data if we wanted to
 router.post('/newAppointment', appointmentController.createAppointment);
 router.post('/followUp', appointmentController.followUpAppointment);
+router.patch('/reschedule/:id',appointmentController.rescheduleAppointment);
+router.patch('/cancel/:id',appointmentController.cancelAppointment);
 router.patch('/update/:id', appointmentController.updateAppointment);
 router.delete('/delete/:id', appointmentController.deleteAppointment);
 
