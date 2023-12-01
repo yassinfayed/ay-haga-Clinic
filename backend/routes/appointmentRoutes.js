2eaf560dafbd290ce03702b2421c06fe571622af
Amir-Ashraf1 <amir.ashraf16502@gmail.com>
2023-12-01 20:06:19 +0200

    RESTRICTIONS

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const { protect } = require('../controllers/authController');
 const authController = require('../controllers/authController');
 const paymentController = require('../controllers/paymentController');
-const { RemoveCircleOutlineRounded } = require('@material-ui/icons');
 router.use(protect);
 
 router.get('/get-patient-appointments', appointmentController.getAllPatientAppointments);
@@ -14,11 +13,11 @@ router.get('/:id', appointmentController.getAppointment);
 
 //This will be deleted in sprints 2 & 3 as it will involve much more complexity, payment refund etc.../ Will be used for now to seed dummy data if we wanted to
 router.post('/newAppointment', appointmentController.createAppointment);
-router.post('/followUp', appointmentController.followUpAppointment);
-router.patch('/reschedule/:id',appointmentController.rescheduleAppointment);
-router.patch('/cancel/:id',appointmentController.cancelAppointment);
+router.post('/followUp', auth.controller.restrictTo(enums.ROLE.DOCTOR,enums.ROLE.PATIENT),appointmentController.followUpAppointment);
+router.patch('/reschedule/:id',auth.controller.restrictTo(enums.ROLE.DOCTOR,enums.ROLE.PATIENT),appointmentController.rescheduleAppointment);
+router.patch('/cancel/:id',auth.controller.restrictTo(enums.ROLE.DOCTOR,enums.ROLE.PATIENT),appointmentController.cancelAppointment);
 router.patch('/update/:id', appointmentController.updateAppointment);
-router.post('/evaluateFollowUp/:id',appointmentController.evaluateFollowUp);
+router.post('/evaluateFollowUp/:id',authController.restrictTo(enums.ROLE.DOCTOR),appointmentController.evaluateFollowUp);
 router.delete('/delete/:id', appointmentController.deleteAppointment);
 
 router.route("/reserveStripe/:id/:price").get(authController.protect, paymentController.getReservationCheckoutSession)
