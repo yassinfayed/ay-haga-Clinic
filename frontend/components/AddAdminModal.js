fd1784e7f58b719255703683c61666add317ea1f
Tarek Zeyad <tarekzeyad31@gmail.com>
2023-12-05 02:42:44 +0200

    fixes

--- frontend/components/AddAdminModal.js
+++ frontend/components/AddAdminModal.js
@@ -4,7 +4,8 @@ import React, { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { useDispatch } from 'react-redux';
 import Image from 'next/image';
-
+import { Alert } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 function CenteredModalAdmin(props) {
   const dispatch = useDispatch();
@@ -17,6 +18,11 @@ function CenteredModalAdmin(props) {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [passwordsMatch, setPasswordsMatch] = useState(true);
+
+  const registerIsFail = useSelector(state => state.registerReducer.error);
+  const [showAlertRegisterSuccess, setShowAlertRegisterSuccess] = useState(false);
+  const [showAlertRegisterFail, setShowAlertRegisterFail] = useState(false);
+  const [showAlertRegisterLoading, setShowAlertRegisterLoading] = useState(false);
 
   const togglePasswordVisibility = (field) => {
     if (field === 'password') {
@@ -44,7 +50,6 @@ function CenteredModalAdmin(props) {
   const formLoad = () => {
     const forms = document.querySelectorAll('.needs-validation')
 
-    // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
@@ -61,24 +66,39 @@ function CenteredModalAdmin(props) {
     e.preventDefault();
 
 
-
-
+    if (passwordValue !== passwordconfirmValue) {
+      return;
+    }
+    
+    setShowAlertRegisterLoading(true);
     dispatch(registerAction({
       "username": usernameValue,
       "password": passwordValue,
       "passwordConfirm": passwordconfirmValue,
       "role": "administrator"
-    }))
-
-    setUsernameValue('');
-    setPasswordValue('');
-    setPasswordConfrimValue('');
-    
-    props.onHide()
-  
+    })).then(() => {
+      setShowAlertRegisterLoading(false);
+      if (registerIsFail) {
+        setShowAlertRegisterFail(true);
+        const timer = setTimeout(() => {
+          setShowAlertRegisterFail(false);
+        }, 3000);
+        return () => clearTimeout(timer);
+      }
+      else {
+        setShowAlertRegisterSuccess(true);
+        const timer = setTimeout(() => {
+          setShowAlertRegisterSuccess(false);
+          props.onHide()
+        }, 200);
+        setUsernameValue('');
+        setPasswordValue('');
+        setPasswordConfrimValue('');
+        return () => clearTimeout(timer);
+      }
+    }
+    );
   }
-
-
   return (
     <Modal
       {...props}
@@ -95,6 +115,22 @@ function CenteredModalAdmin(props) {
         </Modal.Title>
         <div className='underline-Bold mx-auto mt-2 mb-5'></div>
         <h4>{subheader}</h4>
+        {showAlertRegisterLoading && (
+          <Alert variant="primary" className="text-center">
+            Creating admin...
+          </Alert>
+        )}
+        {showAlertRegisterSuccess && (
+          <Alert variant="success" className="text-center">
+            Admin created successfully
+          </Alert>
+        )}
+        {showAlertRegisterFail && (
+          <Alert variant="danger" className="text-center">
+            Failed to create admin
+          </Alert>
+        )}
+
         <form onLoad={formLoad} onSubmit={(e) => handleSubmit(e)} noValidate className='text-semibold mx-3 needs-validation'>
           <div className="mt-3 row">
             <label htmlFor="usernameInput ">Email</label>
