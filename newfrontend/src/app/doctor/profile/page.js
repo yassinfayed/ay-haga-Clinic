69a021b43558769ca4e9ad231c794d0ddc50e7cd
yassinfayed <yassin_fayed@hotmail.com>
2023-12-13 05:19:44 +0200

    profiles

---
+++ newfrontend/src/app/doctor/profile/page.js
@@ -0,0 +1,329 @@
+"use client"
+import React from "react"
+import ChangePassword from "@/components/ChangePassword";
+import { Card } from "@tremor/react";
+import Image from "next/image";
+import ProfilePicture from "@/components/ProfilePicture";
+import { translateDate } from "@/util";
+import { Button } from "@tremor/react";
+import { useDispatch } from "react-redux";
+import { viewDoctorDetails } from "@/app/redux/actions/doctorActions";
+import { useState,useEffect } from "react";
+import { doctorReducer } from "@/app/redux/reducers/doctorReducers";
+import { useSelector } from "react-redux";
+import { doctorAddAvailableDate } from "@/app/redux/actions/doctorActions";
+import { updateDoctor } from "@/app/redux/actions/doctorActions";
+import { Grid } from "@tremor/react";
+import Calendar from "@/components/Calendar";
+import { TextInput } from "@tremor/react";
+import { updateDoctorsReducer } from "@/app/redux/reducers/doctorReducer";
+import { BottomCallout } from "@/components/BottomCallout";
+
+
+
+
+function DoctorProfile () {
+
+
+  const id = JSON.parse(localStorage.getItem("userInfo"))?.data.user.doctor?._id
+  const doctor = useSelector((state) => state.doctorReducer.doctor);
+  const role = JSON.parse(localStorage.getItem("userInfo"))?.data.user.role;
+
+  const docStatus = doctor?.employmentContract?.status;
+
+    const dispatch = useDispatch();
+
+    const {
+      loading: updateLoading,
+      error: updateError,
+      doctor: updateSuccess
+    } = useSelector((state) => state.updateDoctorsReducer);
+
+    useEffect(() => {
+        dispatch(viewDoctorDetails(id));
+      }, [dispatch,updateLoading]);
+
+      const [isEditEmail, setIsEditEmail] = useState(false);
+      const [newHourlyRate, setNewHourlyRate] = useState(doctor?.HourlyRate || "");
+      const [newAffiliation, setNewAffiliation] = useState(doctor?.affiliation || "");
+      const [editedEmail, setEditedEmail] = useState(doctor?.email || "");
+      const [isEditEmployment, setIsEditEmployment] = useState (false);
+      const [newdoctor, setNewDoctor] = useState({});
+      const[show,setShow]=useState(false);
+
+    
+    
+      const handleEditEmail = () => {
+        setIsEditEmail(true);
+      };
+
+      const handleEditEmployment = () => {
+        setIsEditEmployment(true);
+      }
+
+
+      const handleCancelEditEmployment = () => {
+        setIsEditEmployment(false);
+      }
+    
+      const handleCancelEditEmail = () => {
+        setIsEditMode(false);
+        setEditedEmail(doctor?.email || "");
+      };
+    
+      const handleEmailChange = (e) => {
+        setEditedEmail(e.target.value);
+      };
+
+      const handleHourlyRateChange = (e) => {
+        setNewHourlyRate(e.target.value);
+      };
+    
+      const handleAffiliationChange = (e) => {
+        setNewAffiliation(e.target.value);
+      };
+
+
+      const handleSubmitEmployment = () => {
+        setIsEditEmployment(false);
+    
+        let updatedDoctor = { ...newdoctor };
+    
+        if (newHourlyRate) {
+          updatedDoctor.HourlyRate = newHourlyRate;
+        }
+    
+        if (newAffiliation) {
+          updatedDoctor.affiliation = newAffiliation;
+        }
+        setNewDoctor(updatedDoctor);
+        dispatch(updateDoctor(updatedDoctor));
+        setShow(true);
+        setNewHourlyRate("");
+        setNewAffiliation("");
+      };
+
+      const handleSubmitEmail = () => {
+        setIsEditEmail(false);
+    
+        let updatedDoctor = { ...newdoctor };
+    
+        if (editedEmail) {
+          updatedDoctor.email = editedEmail;
+        }
+    
+        setNewDoctor(updatedDoctor);
+        dispatch(updateDoctor(updatedDoctor));
+        setShow(true);
+        setEditedEmail("");
+      };
+
+   
+
+
+
+    return(
+        <div className="h-full overflow-hidden pl-10">
+      <main
+        id="dashboard-main"
+        className=" overflow-auto px-4 py-10"
+      >
+        {docStatus==="accepted" && doctor ? ( 
+            <>
+            <div className="flex flex-wrap gap-x-4 gap-y-8">
+            <div>
+              <ProfilePicture
+                src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?w=826&t=st=1701985608~exp=1701986208~hmac=a251fffe7681073919cbd4f67698f31bfb8d2eab7041e13e952e15e751e4c535"
+                size="90"
+              ></ProfilePicture>
+            </div>
+            <div>
+              <br />
+              <h1 className="text-2xl font-black text-white-800 slide-in-right">
+                Welcome Back {doctor?.name}!
+              </h1>
+            </div>
+          </div>
+          <br />
+  
+          <div numItems={3} className="flex flex-row gap-2">
+            <Card>
+              <div className="flex">
+              <h1 className="text-xl font-bold text-white-200">Personal Information</h1>
+              {!isEditEmail ? (
+                  <div className="ml-5" role="button" onClick={handleEditEmail}>
+                  <span className="">
+                  <Image src="/edit.svg" height={25} width={25}></Image>
+                  </span>
+                </div>
+              ) : (
+                <>
+                <div className="ml-5" role="button" onClick={handleSubmitEmail}>
+                <span className="">
+                <Image src="/tick.svg" height={25} width={25}></Image>
+                </span>
+              </div>
+               <div className="ml-5" role="button" onClick={handleCancelEditEmail}>
+               <span className="">
+               <Image src="/cross.svg" height={25} width={25}></Image>
+               </span>
+             </div>
+             </>
+              )}
+                  
+              </div>
+
+          {updateError && (
+            <BottomCallout
+              message="There was an error updating your information"
+              variant="error"
+              visible={show}
+              setVisible={setShow}
+            />
+          )}
+
+            {updateSuccess && (
+            <BottomCallout
+              message="You have successfully updated your information"
+              variant="success"
+              visible={show}
+              setVisible={setShow}
+            />
+          )}
+
+              <div className="flex mt-[2rem]">
+                <Image src="/user.svg" height={25} width={25}></Image> <p className="ml-3 text-lg">{doctor?.name}</p>
+              </div>
+              <div className="flex mt-5">
+                <Image src="/email.svg" height={25} width={25}></Image>
+                {!isEditEmail ? (
+                <>
+                  <p className="ml-3 text-lg">{doctor?.email}</p>
+                </>
+              ) : (
+                <>
+                  <TextInput
+                    type="email"
+                    value={editedEmail}
+                    onChange={handleEmailChange}
+                    className="ml-3 form-control"
+                  />
+                </>
+              )}
+              </div>
+              <div className="flex mt-5">
+                <Image src="/birthday.svg" height={25} width={25}></Image> <p className="ml-3 text-lg">{translateDate(new Date(doctor?.DateOfbirth))[0]}</p>
+              </div>
+              <div className="flex mt-5">
+                <Image src="/wallet.svg" height={25} width={25}></Image> <p className="ml-3 text-lg">{doctor?.user.wallet} USD</p>
+              </div>
+            </Card>
+            <Card>
+              
+              <div className="flex">
+              <div className="flex">
+              <h1 className="text-xl font-bold text-white-200">Employment</h1>
+              {!isEditEmployment ? (
+                  <div className="ml-5" role="button" onClick={handleEditEmployment}>
+                  <span className="">
+                  <Image src="/edit.svg" height={25} width={25}></Image>
+                  </span>
+                </div>
+              ) : (
+                <>
+                <div className="ml-5" role="button" onClick={handleSubmitEmployment}>
+                <span className="">
+                <Image src="/tick.svg" height={25} width={25}></Image>
+                </span>
+              </div>
+               <div className="ml-5" role="button" onClick={handleCancelEditEmployment}>
+               <span className="">
+               <Image src="/cross.svg" height={25} width={25}></Image>
+               </span>
+             </div>
+             </>
+              )}
+                  
+              </div>
+              </div>
+              <div className="flex mt-[2rem]">
+                <Image src="/speciality.svg" height={25} width={25}></Image> <p className="ml-3 text-lg">{doctor?.speciality}</p>
+              </div>
+              <div className="flex mt-5">
+                <Image src="/hospital.svg" height={25} width={25}></Image>
+                {!isEditEmployment ? (
+                <>
+                  <p className="ml-3 text-lg">{doctor?.affiliation}</p>
+                </>
+              ) : (
+                <>
+                  <TextInput
+                    type="text"
+                    value={newAffiliation}
+                    onChange={handleAffiliationChange}
+                    className="ml-3 form-control"
+                  />
+                </>
+              )}
+              </div>
+              <div className="flex mt-5">
+                <Image src="/book.svg" height={25} width={25}></Image> <p className="ml-3 text-lg">{doctor?.educationalbackground}</p>
+              </div>
+              <div className="flex mt-5">
+                <Image src="/hourly.svg" height={25} width={25}></Image>
+                {!isEditEmployment ? (
+                <>
+                  <p className="ml-3 text-lg">{doctor?.HourlyRate}</p>
+                </>
+              ) : (
+                <>
+                  <TextInput
+                    type="text"
+                    value={newHourlyRate}
+                    onChange={handleHourlyRateChange}
+                    className="ml-3 form-control"
+                  />
+                </>
+              )}
+              </div>
+            </Card>
+            <div className="">
+              {role === "doctor" ? (
+                   <ChangePassword />
+              ):(
+                  {}
+              )}
+                 
+            </div>
+            
+          </div>
+          <Grid className="mt-2">
+            <div>
+                <br />
+            <hr />
+            <br />
+            <h1 className="text-3xl font-bold text-white-200">My Available Slots</h1>
+            </div>
+          <div className="flex flex-row gap-2">
+            <Calendar availableSlots={doctor?.availableDates}></Calendar>
+          </div>
+        </Grid>
+          </>
+        ) : (
+            null
+        )}
+
+        { docStatus!=='accepted' ? (
+                null
+            ) : (
+                null
+            )}
+        
+
+      </main>
+    </div>
+    )
+
+}
+
+export default DoctorProfile;
---
+++ newfrontend/src/components/Calendar.js
@@ -0,0 +1,276 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { doctorAddAvailableDate } from "@/app/redux/actions/doctorActions";
+import { doctorAddAvailableDateReducer } from "@/app/redux/reducers/doctorReducer";
+import { BottomCallout } from "./BottomCallout";
+import { useSelector } from "react-redux";
+import { TimePicker } from 'react-ios-time-picker';
+import { viewDoctorDetails } from "@/app/redux/actions/doctorActions";
+
+function Calendar({ availableSlots }) {
+  const id = JSON.parse(localStorage.getItem("userInfo"))?.data.user.doctor?._id
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [isTimePickerOpen, setIsTimePickerOpen] = useState(false);
+  const [clickedDay, setClickedDay] = useState(null);
+  const [events, setEvents] = useState({});
+  const [show,setShow] = useState(false);
+  const dispatch = useDispatch();
+
+
+  const {
+    loading: addLoading,
+    error: addError,
+    availableDates: addSuccess
+  } = useSelector((state) => state.doctorAddAvailableDateReducer);
+
+  useEffect(() => {
+    const formattedEvents = availableSlots.reduce((acc, slot) => {
+      const date = new Date(slot);
+      const day = date.getDate();
+      const time = date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+      const month = date.getMonth();
+      const year = date.getFullYear();
+      const eventKey = `${day}-${month}-${year}`;
+      acc[eventKey] = [...(acc[eventKey] || []), { time, formattedDate: date.toISOString() }];
+      return acc;
+    }, {});
+    setEvents(formattedEvents);
+  }, [availableSlots, currentDate,addLoading]);
+
+
+  const daysInMonth = (year, month) => new Date(year, month + 1, 0).getDate();
+  const firstDayOfMonth = (year, month) => new Date(year, month, 1).getDay();
+
+  const prevMonth = () => {
+    setCurrentDate((prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1));
+  };
+
+  const nextMonth = () => {
+    setCurrentDate((prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1));
+  };
+
+  const handleDayClick = (day) => {
+    setClickedDay(day);
+    setIsTimePickerOpen(true);
+  };
+
+  const displayLocalTime = (utcTime) => {
+    const date = new Date(utcTime);
+    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", timeZone: "UTC" });
+  };
+
+  const handleTimeSelected = (time) => {
+    setIsTimePickerOpen(false);
+    if (time) {
+      const eventKey = `${clickedDay}-${currentDate.getMonth()}-${currentDate.getFullYear()}`;
+      const day = clickedDay;
+      const month = currentDate.getMonth();
+      const year = currentDate.getFullYear();
+      const selectedDate = new Date(Date.UTC(year, month, day));
+      const selectedTime = new Date(selectedDate.setUTCHours(parseInt(time.split(":")[0]), parseInt(time.split(":")[1])));
+      dispatch(doctorAddAvailableDate({ availableDate: selectedTime }))
+        .then(() => {
+          // Dispatch viewDoctorDetails after successfully adding available dates
+          dispatch(viewDoctorDetails(id))
+            .then(() => {
+              // Fetch the updated doctor details and set the events
+              const updatedDoctor = useSelector((state) => state.doctorReducer.doctor);
+              const formattedEvents = updatedDoctor?.availableDates?.reduce((acc, slot) => {
+                const date = new Date(slot);
+                const slotDay = date.getDate();
+                const slotTime = date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+                const slotMonth = date.getMonth();
+                const slotYear = date.getFullYear();
+                const slotEventKey = `${slotDay}-${slotMonth}-${slotYear}`;
+                acc[slotEventKey] = [...(acc[slotEventKey] || []), { time: slotTime, formattedDate: date.toISOString() }];
+                return acc;
+              }, {});
+              setEvents(formattedEvents);
+              setShow(true);
+            })
+            .catch((error) => {
+              console.error("Error fetching updated doctor details:", error);
+              // Handle error fetching updated details
+            });
+        })
+        .catch((error) => {
+          console.error("Error adding available date:", error);
+          // Handle error adding available date
+        });
+    }
+  };
+
+  
+
+  const renderCalendar = () => {
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth();
+
+    const totalDays = daysInMonth(year, month);
+    const startingDay = firstDayOfMonth(year, month);
+
+    let days = [];
+
+    // Add empty cells for the days before the 1st of the month
+    for (let i = 0; i < startingDay; i++) {
+      days.push(<td key={`empty-${i}`} className="border p-1 w-10 h-40"></td>);
+    }
+
+    // Add days of the month
+    for (let day = 1; day <= totalDays; day++) {
+        const eventKey = `${day}-${currentDate.getMonth()}-${currentDate.getFullYear()}`;
+        days.push(
+          <td
+            key={day}
+            className="border p-1 h-40 w-10 overflow-auto transition cursor-pointer duration-500 ease hover:bg-gray-300"
+            onClick={() => handleDayClick(day)}
+          >
+            <div className=" h-40 mx-auto w-40 mx-auto overflow-hidden">
+              <div className="top h-5 w-full">
+                <span className="text-white-500">{day}</span>
+              </div>
+              <div className="bottom flex-grow h-30 py-1 w-full cursor-pointer">
+                {events[eventKey] &&
+                  events[eventKey]
+                    .filter((event) => {
+                      const eventMonth = new Date(event.formattedDate).getMonth();
+                      const eventYear = new Date(event.formattedDate).getFullYear();
+                      return eventMonth === currentDate.getMonth() && eventYear === currentDate.getFullYear();
+                    })
+                    .map((event, index) => (
+                      <div key={index} className="bg-blue-400 text-white rounded  text-2xl mb-1">
+                        <span className="event-name">{displayLocalTime(event.formattedDate)}</span>
+                      </div>
+                    ))}
+              </div>
+            </div>
+          </td>
+        );
+      }
+    // Wrap days in rows
+    let rows = [];
+    let cells = [];
+
+    days.forEach((day, index) => {
+      if (index % 7 !== 0 || index === 0) {
+        cells.push(day);
+      } else {
+        rows.push(<tr key={index / 7}>{cells}</tr>);
+        cells = [];
+        cells.push(day);
+      }
+    });
+
+    // Add the last row
+    rows.push(<tr key={days.length / 7}>{cells}</tr>);
+
+    // Define an array of day names
+    const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    // Create header row with day names
+    const headerRow = (
+      <tr>
+        {dayNames.map((dayName) => (
+          <th key={dayName} className="border p-1 w-10 h-10 text-center">
+            {dayName}
+          </th>
+        ))}
+      </tr>
+    );
+
+    return (
+      <tbody>
+        {headerRow}
+        {rows}
+      </tbody>
+    );
+  };
+
+
+  return (
+    <>
+      <div className="container mx-auto mt-10">
+        <div className="wrapper rounded shadow w-full ">
+          <div className="header flex justify-between border-b p-2">
+            <span className="text-lg font-bold">
+            {currentDate.toLocaleString("default", { month: "long", year: "numeric" })}
+            </span>
+            {isTimePickerOpen &&  <TimePicker onChange={(e) => handleTimeSelected(e.target.value)} /> }
+            <div className="buttons">
+            <button className="p-1" onClick={prevMonth}>
+            <svg
+              width="1em"
+              fill="gray"
+              height="1em"
+              viewBox="0 0 16 16"
+              className="bi bi-arrow-left-circle"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M8.354 11.354a.5.5 0 0 0 0-.708L5.707 8l2.647-2.646a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708 0z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M11.5 8a.5.5 0 0 0-.5-.5H6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 .5-.5z"
+              />
+            </svg>
+          </button>
+          <button className="p-1" onClick={nextMonth}>
+            <svg
+              width="1em"
+              fill="gray"
+              height="1em"
+              viewBox="0 0 16 16"
+              className="bi bi-arrow-right-circle"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M7.646 11.354a.5.5 0 0 1 0-.708L10.293 8 7.646 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M4.5 8a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
+          </button>
+            </div>
+          </div>
+          <table className="w-full">
+            <thead>
+              <tr>{/* Header row for day names */}</tr>
+            </thead>
+            {renderCalendar()}
+          </table>
+          {addError && (
+            <BottomCallout
+              message="There was an error updating your schedule"
+              variant="error"
+              visible={show}
+              setVisible={setShow}
+            />
+          )}
+          {addSuccess && (
+            <BottomCallout
+              message="You have successfully added a slot to your schedule"
+              variant="success"
+              visible={show}
+              setVisible={setShow}
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Calendar;
