ba46999549cb188a30e160c2cfb40460d994d12d
yassinfayed <yassin_fayed@hotmail.com>
2023-12-14 16:11:06 +0200

    chat-video-conflicts

--- backend/app.js
+++ backend/app.js
@@ -66,60 +66,6 @@ io.on('connection', (socket) => {
   });
 });
 
-const ioChat = socketIO(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ['GET', 'POST'],
-  },
-}).of('/chat');
-
-
-let users = [];
-
-const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
-  console.log(users);
-};
-
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
-
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
-};
-
-ioChat.on("connection", (socket) => {
-  console.log("a user connected.");
-
-  
-  socket.on("addUser", (userId) => {
-    console.log("we will add user");
-    console.log("USER:",userId,"Socket:",socket.id);
-    addUser(userId, socket.id);
-    ioChat.emit("getUsers", users);
-  });
-
-  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    const user = getUser(receiverId);
-    if(user && user.socketId){
-      ioChat.to(user.socketId).emit("getMessage", {
-        senderId,
-        text,
-      });
-    }
-  });
-
-  //when disconnect
-  socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-    removeUser(socket.id);
-    ioChat.emit("getUsers", users);
-  });
-});
-
-
 // 1) GLOBAL MIDDLEWARES
 // app.use(cors());
 
--- newfrontend/src/app/patient/chat/page.js
+++ newfrontend/src/app/patient/chat/page.js
@@ -24,7 +24,7 @@ export default function Messenger() {
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("http://localhost:8000/chat");
+    socket.current = io("http://localhost:8900");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
         sender: data.senderId,
