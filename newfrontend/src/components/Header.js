bf29b6c42a0a5bd7668616dbec29bca1d9fed411
omartamer-git <omartamer02@hotmail.com>
2023-12-17 05:11:15 +0200

    Logo and footer and error

--- newfrontend/src/components/Header.js
+++ newfrontend/src/components/Header.js
@@ -4,14 +4,14 @@ import { logoutAction } from "@/app/redux/actions/authActions";
 import { useDispatch } from "react-redux";
 
 
-const Header = ({logoutOnly}) => {
+const Header = ({ logoutOnly }) => {
   const dispatch = useDispatch();
 
   return (
     <header className="shadow-md py-4 px-4 sm:px-10 font-sans-serif min-h-70px">
-      {(logoutOnly!=true) ? (<div className="flex flex-wrap items-center justify-between gap-5 relative">
+      {(logoutOnly != true) ? (<div className="flex flex-wrap items-center justify-between gap-5 relative">
         {/* replace with clinic logo */}
-        <a href="javascript:void(0)"> 
+        <a href="javascript:void(0)">
           <div className="flex">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
@@ -31,7 +31,7 @@ const Header = ({logoutOnly}) => {
             </a>
           </button>
           <button className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff] ml-3">
-          <a href="/signup/patient">
+            <a href="/signup/patient">
               Sign Up
             </a>
           </button>
@@ -94,34 +94,40 @@ const Header = ({logoutOnly}) => {
         </ul>
       </div>) : (
         <div className="flex flex-wrap items-center justify-between gap-5 relative">
-        {/* replace with clinic logo */}
-        <a href="javascript:void(0)"> 
-          <img
-            src="https://readymadeui.com/readymadeui.svg"
-            alt="logo"
-            className="w-36"
-          />
-        </a>
-        <div className="flex lg:order-1 max-sm:ml-auto">
-          <button onClick={() => dispatch(logoutAction())} className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
+          {/* replace with clinic logo */}
+          <a href="javascript:void(0)">
+            <div className="flex">
+              <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="h-8"
+                alt="Clinic Logo"
+              />
+
+              <span className="ms-2 self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                Vite Clinics
+              </span>
+            </div>
+          </a>
+          <div className="flex lg:order-1 max-sm:ml-auto">
+            <button onClick={() => dispatch(logoutAction())} className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
               Logout
-          </button>
-          <button className="lg:hidden ml-7">
-            <svg
-              className="w-7 h-7"
-              fill="#000"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
+            </button>
+            <button className="lg:hidden ml-7">
+              <svg
+                className="w-7 h-7"
+                fill="#000"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
       )}
     </header>
   );
